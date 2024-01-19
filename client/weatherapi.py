4f96be9aebfa3e94e8be025aef1127a8cec1b8da
Ahmad Nurjalal <anurjalal@protonmail.com>
2024-01-20 06:37:58 +0700

    fix: use responses lib instead of urllib3 to handle error deployment in pythonanywhere

--- client/weatherapi.py
+++ client/weatherapi.py
@@ -1,18 +1,10 @@
-import json
 import os
-
-import urllib3
+import requests
 
 API_KEY = os.getenv("WEATHERAPI_KEY")
 base_url = "https://api.weatherapi.com/v1/forecast.json"
-client = urllib3.PoolManager()
 
 
 def get_forcast(location, n_days):
-    r = client.request("POST",
-                       base_url,
-                       fields=dict(key=API_KEY, q=location, days=n_days)
-                       )
-    response = r.data.decode('utf-8')
-    result = json.loads(response)
-    return result
+    response = requests.post("https://api.weatherapi.com/v1/forecast.json", dict(key=API_KEY, q=location, days=n_days))
+    return response.json()
