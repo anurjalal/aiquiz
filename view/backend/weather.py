c8632e1f3cddcb049f29a9b6036983db4aec7677
Ahmad Nurjalal <anurjalal@protonmail.com>
2024-01-20 07:16:04 +0700

    feat: add request validation on weatherapi

--- view/backend/weather.py
+++ view/backend/weather.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, request, render_template
 from middleware.authenticator import is_valid_token
 from service import weather
+from webargs import fields
+from webargs.flaskparser import use_args
 
 weather_blueprint = Blueprint("weather", __name__)
 
+weatherapi_args = {"location": fields.Str(validate=lambda p: 0 < len(p) <= 255),}
+
 
 @weather_blueprint.route('/weather_3day', methods=['GET'])
-def get_weather_3day():
+@use_args(weatherapi_args, location="form")
+def get_weather_3day(args):
     location = request.args.get('location')
     forcast = weather.get_3day_forecast(location)
     return render_template("home.html",
